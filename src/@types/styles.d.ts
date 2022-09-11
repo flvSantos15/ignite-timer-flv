aaa58b3aac3973e93c57f4ce8e8826aedaf8753b
flvSantos15 <flvSantos300@gmail.com>
2022-09-11 17:38:29 -0300

    chore: cores e fonte

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -3,6 +3,6 @@ import { defaultTheme } from "../styles/themes/default";
 
 type ThemeType = typeof defaultTheme;
 
-declare module "styled-componentes" {
+declare module "styled-components" {
   export interface DefaultTheme extends ThemeType { }
 }
