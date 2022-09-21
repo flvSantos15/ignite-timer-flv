99ec0d7378489388f7ef5b5533de1cdb03c3918a
flvSantos15 <flvSantos300@gmail.com>
2022-09-21 06:28:59 -0300

    chore: separating components

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,39 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+
+export function NewCycleForm() {
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        type="text"
+        placeholder="Dê um nome para o seu projeto"
+        list="task-suggestions"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', {
+          valueAsNumber: true,
+        })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
