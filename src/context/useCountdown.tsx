b58eba305736d546a7561c04981b8ac67c192b43
flvSantos15 <flvsantos300@gmail.com>
2022-09-27 20:49:21 -0300

    chore: salvando estado no storage

--- src/context/useCountdown.tsx
+++ src/context/useCountdown.tsx
@@ -1,7 +1,9 @@
+import { differenceInSeconds } from 'date-fns'
 import {
   createContext,
   ReactNode,
   useContext,
+  useEffect,
   useReducer,
   // eslint-disable-next-line prettier/prettier
   useState
@@ -37,16 +39,36 @@ interface CyclesProviderData {
 export const CyclesContext = createContext({} as CyclesContextData)
 
 export const CyclesProvider = ({ children }: CyclesProviderData) => {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@ignite-timer:cycles-state-1.0.0',
+      )
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+    },
+  )
 
   const { cycles, activeCycleId } = cyclesState
 
   const activeCycle = cycles.find((cycles) => cycles.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    // esse foi um exemplo de otimização
+    // na aula teve um problema de demora pra setar o segundos vindo do storage
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle?.startDate))
+    }
+
+    return 0
+  })
 
   const markCycleAsFinishedAsFinished = () => {
     dispatch(markCurrentCycleAsFinishedAction())
@@ -74,6 +96,12 @@ export const CyclesProvider = ({ children }: CyclesProviderData) => {
   const interruptCurrentCycle = () => {
     dispatch(interruptCurrentCycleAction())
   }
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
 
   return (
     <CyclesContext.Provider
